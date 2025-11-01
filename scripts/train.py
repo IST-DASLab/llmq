68bbe80b6b9f83dc998b416ef48cb6dc0a331fec
Erik Schultheis <erik.schultheis@ist.ac.at>
2025-11-01 03:46:56 +0100

    update

--- scripts/train.py
+++ scripts/train.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S uv run --script
 # /// script
 # requires-python = ">=3.12"
-# dependencies = ["pyllmq", "tqdm", "numpy"]
+# dependencies = ["pyllmq", "tqdm", "numpy", "wandb"]
 # ///
 
 import pyllmq
