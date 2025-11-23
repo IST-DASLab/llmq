1104417e264018dcdc80c321d6f8edc11f1b525f
Erik Schultheis <erik.schultheis@ist.ac.at>
2025-11-23 02:35:39 +0100

    middle

--- src/models/llama_model.cpp
+++ src/models/llama_model.cpp
@@ -408,10 +408,9 @@ void LLamaModel::backward(Tensor inputs, Tensor targets, NCCLCommunicator& comm,
         auto& weights = Parameters->get_block(l, main_stream);
         auto& d_acts = rs->DActs.at(l);
         Tensor residual = l == 0 ? rs->Encoded : rs->get_res_ffn(l - 1, main_stream);
-        if(last_step)
-            CUDA_CHECK(cudaStreamWaitEvent(main_stream, test_event, 0));
         trace_or_execute_cuda_graph([&]() {
             _recompute_block(weights, rs->Acts[l], residual);
+            CUDA_CHECK(cudaStreamWaitEvent(main_stream, test_event, 0));
             _backward_block(accumulate, weights, dw, rs->Acts[l], rs->DActs[l]);
             }, main_stream, rs->BackwardBlockGraph, rs->Options.UseCudaGraphs);
         if(l > 0) {
