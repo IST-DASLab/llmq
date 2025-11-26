9d3045a88e75196bb6bc5e7a45b548ef1fb19176
Erik Schultheis <erik.schultheis@ist.ac.at>
2025-11-26 20:46:51 +0100

    added llama-7b configs

--- src/models/llama_config.cpp
+++ src/models/llama_config.cpp
@@ -136,6 +136,48 @@ static LLamaConfig create_qwen25_config(int hidden_size, int intermediate_size, 
     };
 }
 
+static LLamaConfig create_llama2_config(int hidden_size, int intermediate_size, int heads, int depth, ETensorDType dtype) {
+    return {
+        .Architecture = LLamaConfig::LLAMA,
+        .BosTokenId = 1,
+        .EosTokenId = 2,
+        .PadTokenId = 0,
+        .HiddenSize = hidden_size,
+        .IntermediateSize = intermediate_size,
+        .VocabSize = 32000,
+        .NumQueryHeads = heads,
+        .NumKeyValHeads = heads,
+        .NumLayers = depth,
+        .MaxPositionEmbeddings = 4096,
+        .RopeTheta = 10000.f,
+        .RmsNormEps = 1e-05f,
+        .TiedWordEmbeddings = false,
+        .UseQKVBias = false,
+        .DType = dtype
+    };
+}
+
+static LLamaConfig create_llama3_config(int hidden_size, int intermediate_size, int q_heads, int kv_heads, int depth, ETensorDType dtype) {
+    return {
+        .Architecture = LLamaConfig::LLAMA,
+        .BosTokenId = 128000,
+        .EosTokenId = 128001,
+        .PadTokenId = 128255,
+        .HiddenSize = hidden_size,
+        .IntermediateSize = intermediate_size,
+        .VocabSize = 128256,
+        .NumQueryHeads = q_heads,
+        .NumKeyValHeads = kv_heads,
+        .NumLayers = depth,
+        .MaxPositionEmbeddings = 4096,
+        .RopeTheta = 500000.f,
+        .RmsNormEps = 1e-05f,
+        .TiedWordEmbeddings = false,
+        .UseQKVBias = false,
+        .DType = dtype
+    };
+}
+
 LLamaConfig create_config_from_name(std::string_view name, ETensorDType dtype) {
     if(iequals(name, "Qwen2.5-0.5B")) {
         return create_qwen25_config(896, 4861, 14, 2, 24, 1e-06f, true, dtype);
@@ -151,7 +193,12 @@ LLamaConfig create_config_from_name(std::string_view name, ETensorDType dtype) {
         return create_qwen25_config(5120, 27648, 40, 8, 64, 1e-05f, false, dtype);
     } else if(iequals(name, "Qwen2.5-72B")) {
         return create_qwen25_config(8192, 29568, 64, 8, 80, 1e-05f, false, dtype);
+    } else if (iequals(name, "llama-2-7b")) {
+        return create_llama2_config(4096, 11008, 32, 32, dtype);
+    } else if (iequals(name, "llama-2-13b")) {
+        return create_llama2_config(5120, 13824, 40, 40, dtype);
+    } else if (iequals(name, "llama-3-8b")) {
+        return create_llama3_config(4096, 14336, 32, 8, 32, dtype);
     }
-    // TODO add llama models
     throw std::runtime_error(fmt::format("unknown model name {}", name));
 }
