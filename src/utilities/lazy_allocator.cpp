9f9da2a2db71fe614ee98dfa09947aa296152aac
Erik Schultheis <erik.schultheis@ist.ac.at>
2025-12-08 02:22:32 +0100

    lazy allocator

---
+++ src/utilities/lazy_allocator.cpp
@@ -0,0 +1,54 @@
+// Copyright (c) 2025, IST Austria, developed by Erik Schultheis
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "lazy_allocator.h"
+#include "utilities/tensor.h"
+#include "utilities/allocator.h"
+#include "utilities/stack.h"
+
+void LazyAllocator::allocate(Tensor* target, ETensorDType dtype, const std::vector<long>& shape) {
+    target->Data = nullptr;
+    target->Rank = shape.size();
+    target->DType = dtype;
+    std::copy(shape.begin(), shape.end(), target->Sizes.begin());
+    mTargets.push_back(target);
+}
+
+Tensor LazyAllocator::commit(TensorAllocator& storage, EAllocationType type, const char* name) {
+    std::size_t total_size = 0;
+    constexpr std::size_t page_size = 4096;
+    for(auto& target: mTargets) {
+        std::size_t tgt_size = div_ceil(target->bytes(), page_size) * page_size;
+        total_size += tgt_size;
+    }
+
+    Tensor backing = storage.allocate(ETensorDType::BYTE, name, type, {(long)total_size});
+    auto* ptr = backing.get<std::byte>();
+    for(auto& target: mTargets) {
+        target->Data = ptr;
+        target->Device = backing.Device;
+        ptr += div_ceil(target->bytes(), page_size) * page_size;
+    }
+
+    return backing;
+}
+
+Tensor LazyAllocator::commit(DeviceMemoryStack& storage, const char* name) {
+    std::size_t total_size = 0;
+    constexpr std::size_t page_size = 4096;
+    for(auto& target: mTargets) {
+        std::size_t tgt_size = div_ceil(target->bytes(), page_size) * page_size;
+        total_size += tgt_size;
+    }
+
+    Tensor backing = storage.allocate(ETensorDType::BYTE, {(long)total_size}, name);
+    auto* ptr = backing.get<std::byte>();
+    for(auto& target: mTargets) {
+        target->Data = ptr;
+        target->Device = backing.Device;
+        ptr += div_ceil(target->bytes(), page_size) * page_size;
+    }
+
+    return backing;
+}
