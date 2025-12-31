722d14cf963b7e11f1f79761393d7c4672403f08
Erik Schultheis <erik.schultheis@ist.ac.at>
2025-12-31 14:39:32 +0100

    adding a new, minimal TensorContainer interface

--- src/utilities/tensor_container.h
+++ src/utilities/tensor_container.h
@@ -7,9 +7,31 @@
 #define LLMQ_SRC_UTILS_TENSOR_CONTAINER_H
 
 #include <functional>
+#include <cstddef>
 #include <string>
 
+class Tensor;
 class TensorShard;
+
+class SimpleTensorContainer {
+public:
+    virtual std::size_t num_tensors() const noexcept = 0;
+    virtual const Tensor& get_tensor(unsigned idx) const = 0;
+
+    Tensor& get_tensor(unsigned idx) {
+        return const_cast<Tensor&>(const_cast<const SimpleTensorContainer&>(*this).get_tensor(idx));
+    }
+protected:
+    ~SimpleTensorContainer() = default;
+};
+
+//! \brief Apply the given function to all _non-empty_ tensors in `container`
+void visit(const std::function<void(Tensor&)>& func, SimpleTensorContainer& container);
+
+//! \brief Apply the given function to all _non-empty_ tensors in `a` and `b`.
+//! \details `a` and `b` must have the same number of tensors, and empty tensors must be at the same indices
+//! in both containers.
+void visit(const std::function<void(Tensor&, Tensor&)>& func, SimpleTensorContainer& a, SimpleTensorContainer& b);
 
 class ITensorContainer {
   public:
