09c8391324cd9c713f783da8d398e1e20eec9bff
Erik Schultheis <erik.schultheis@ist.ac.at>
2026-01-09 09:05:46 +0100

    fixes

--- src/kernels/fused_classifier.cu
+++ src/kernels/fused_classifier.cu
@@ -210,7 +210,7 @@ void fused_classifier_dispatch(Type* logits, float* losses, float* lse,
     CUDA_CHECK(cudaGetDevice(&device));
     int major;
     CUDA_CHECK(cudaDeviceGetAttribute(&major, cudaDevAttrComputeCapabilityMajor, device));
-    if ((major == 9 || major == 10) && write_dlogits == true && V % 8 == 0) {
+    if ((major == 9 || major == 10) && write_dlogits == true && V % (8 * 16 / sizeof(Type)) == 0) {
         // while the TMA implementation also works on CC12, it doesn't seem to bring performance
         // benefits so far.
         fused_classifier_tma(logits, losses, lse, dloss, targets, z_reg, BT, V, P, stream);
